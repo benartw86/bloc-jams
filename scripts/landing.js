c8c19c72e993e0da8e25c132ca61a68e455ae17e
Ben White <masinfa@gmail.com>
2016-11-16 18:30:49 -0500

    Solution to Assignment 9 applied

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,35 +1,27 @@
-var points = document.getElementsByClassName('point');   //establish node list
+var pointsArray = document.getElementsByClassName('point');   //establish node list
+
+var revealPoint = function(index) {  
+                point.style.opacity = .5;
+                point.style.transform = "scaleX(1) translateY(0)";
+                point.style.msTransform = "scaleX(1) translateY(0)";
+                point.style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
+    
 
 var animatePoints = function(points) {  
-                 
-                points.style.opacity = .5;
-                points.style.transform = "scaleX(1) translateY(0)";
-                points.style.msTransform = "scaleX(1) translateY(0)";
-                points.style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
+               
+            forEach(points, revealPoint);  //points is paramater of animatePoints  which will use revealPoint as callback to animate since it    
         
-        points.forEach(animatePoints); //use forEach block, callback animate points
+        }
 };
 
- 
-    
-/*    for (var i = 0; i < points.length; i++) {
-        
-                points[i].style.opacity = .5;
-                points[i].style.transform = "scaleX(1) translateY(0)";
-                points[i].style.msTransform = "scaleX(1) translateY(0)";
-                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-           
-        }
-        
-    
-};  */
+
  
 
  window.onload = function() {
      
       if (window.innerHeight > 950) {
-         animatePoints(points);
+         animatePoints(pointsArray);
      }
      
      var sellingPoints = document.getElementsByClassName('selling-points')[0];
@@ -37,7 +29,7 @@ var animatePoints = function(points) {
 
      window.addEventListener("scroll", function(event) { //listener, event, handler
          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(points);   
+             animatePoints(pointsArray);   
          }
      });
  }
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,3 +1,13 @@
+ function forEach(array, callback) {   //create function to iterate over array
+    for (var i = 0; i<array.length; i++) {     // this function will replace
+           callback(array[i]);
+    }
+}     // invoke the callback to run code on the different elements that will be               iterated over in the array  
+
+
+
+
+
 var pointsArray = document.getElementsByClassName('point');
 
 var myFunc = function(text, callback) {
