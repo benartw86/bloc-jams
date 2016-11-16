882f90fd99b7f98c6fda330754a1fa418a8a2f0d
Ben White <masinfa@gmail.com>
2016-11-15 19:14:26 -0500

    More changes to checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -17,10 +17,14 @@ var animatePoints = function(points) {
 
 
  window.onload = function() {
+     
+      if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     
      var sellingPoints = document.getElementsByClassName('selling-points')[0];
      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-     
-     
+
      window.addEventListener("scroll", function(event) { //listener, event, handler
          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(points);   
