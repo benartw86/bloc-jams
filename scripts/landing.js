72047a0e288c67fe862ad9260daf3969c8f45bcf
Ben White <masinfa@gmail.com>
2016-11-27 12:17:25 -0500

    Checkpoint 16 jQuery landing.js finished

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,38 +1,32 @@
-var pointsArray = document.getElementsByClassName('point');   //establish node list
 
-var revealPoint = function(point) {  
-                point.style.opacity = .5;
-                point.style.transform = "scaleX(1) translateY(0)";
-                point.style.msTransform = "scaleX(1) translateY(0)";
-                point.style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
-    
 
-var animatePoints = function(points) {  
-               
-            forEach(points, revealPoint);  //points is paramater of animatePoints  which will use revealPoint as callback to animate since it    
-        
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+ $.each($('.point'), revealPoint);
 };
 
-
- 
-
- window.onload = function() {
+ $(window).load(function() {
      
-      if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+      if ($(window).height() > 950) {
+         animatePoints();
      }
      
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-     window.addEventListener("scroll", function(event) { //listener, event, handler
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+    $(window).scroll(function(event) { //listener, event, handler
+         if($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
      });
- }
+ });
  
  
  
  //the listener is activated by the event which also triggers the event
+                
+                 
